2d8bbd100d3dadb0aa768bd1e25f3bd3c7883f45
gyts <gytis.stanciukas@gmail.com>
2016-01-30 01:33:34 +0200

    head

--- Chop Chop/Assets/StairManager.cs
+++ Chop Chop/Assets/StairManager.cs
@@ -7,28 +7,30 @@ public class StairManager : MonoBehaviour {
 	List<GameObject> stairs;
 	int step = 0;
 	int maxStepCount = 50;
+	float objWidth = 1f;
+
+	public GameObject head;
 	void Start () {
 		stairs = new List<GameObject> ();
 		for (int i = -5; i < 5; i++) {
-			var a = (GameObject)Instantiate (stair, new Vector3 (i, -1 * i, 0), Quaternion.Euler (new Vector3 (0, 0, 0)));
-			if (i == -5)
-				a.tag = "tag0";
+			var a = (GameObject)Instantiate (stair, new Vector3 (objWidth * i, -objWidth * i, 0), Quaternion.Euler (new Vector3 (0, 0, 0)));
 			stairs.Add (a);
 		}
+		Instantiate (head, new Vector3 (0, 1, 0), Quaternion.Euler (new Vector3 (0, 0, 0)));
 	}
 
 	void Update () {
 		step++;
 		foreach (GameObject stair in stairs) {
 			Vector3 pos = stair.transform.position;
-			pos.x -= 1f / maxStepCount;
-			pos.y += 1f / maxStepCount;
+			pos.x -= objWidth / maxStepCount;
+			pos.y += objWidth / maxStepCount;
 			stair.transform.position = pos;
 		}
 		if (step == maxStepCount - 1) {
 			var topStair = stairs [0];
 			for (int i = -5; i < 5; i++) {
-				stairs [i + 5].transform.position = new Vector3 (i, -1 * i, 0);
+				stairs [i + 5].transform.position = new Vector3 (objWidth * i, -objWidth * i, 0);
 			}
 			//topStair.transform.position = new Vector3 (5, -5, 0);
 			//stairs.Insert (9, topStair);
@@ -39,5 +41,8 @@ public class StairManager : MonoBehaviour {
 			//stairs.Add (a);
 			step = 0;
 		}
+
+		Debug.Log (maxStepCount / 2 * step);
+		head.transform.position = new Vector3 (0, 1 / (maxStepCount / 2), 0);
 	}
 }
