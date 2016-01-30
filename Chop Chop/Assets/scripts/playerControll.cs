16b3cd32e40bb959395de4e29743403d1084764c
ignas <ignasjas3@gmail.com>
2016-01-30 15:24:13 +0200

    g

--- Chop Chop/Assets/scripts/playerControll.cs
+++ Chop Chop/Assets/scripts/playerControll.cs
@@ -7,6 +7,8 @@ public class playerControll : MonoBehaviour {
 	public float minHeigth = 2.5f;
 	public float maxheigth = 12f;
 	public ParticleSystem blood;
+	private bool canJump = true;
+	public float maxVelocity = 30;
 
 	// Use this for initialization
 	void Start () {
@@ -14,19 +16,20 @@ public class playerControll : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
-
-
-		if (Input.GetKeyDown ("up")) {
-			GetComponent<Rigidbody> ().AddForce (new Vector3 (0,600,0));
+		GetComponent<Rigidbody> ().AddForce (new Vector3 (2,0,0));
+		GetComponent<Rigidbody> ().velocity = Vector3.ClampMagnitude (GetComponent<Rigidbody> ().velocity, maxVelocity);
+		if (Input.GetKeyDown ("up") && canJump == true) {
+			GetComponent<Rigidbody> ().AddForce (new Vector3 (0,1100,0));
+			canJump = false;
 		}
 
 	}
 	void OnCollisionEnter(Collision col) {
 		GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 250, 0));
 		blood.Emit (30);
-
+		canJump = true;
 		if (col.gameObject.tag == "vaze") {
 			Instantiate (Resources.Load ("vazeCrash"), transform.position, transform.rotation);
 			Destroy (col.gameObject);
---
+++ Chop Chop/Assets/scripts/turret.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turret : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Translate(Vector3.right * -30 * Time.deltaTime);
+		if (transform.position.z < -20) {
+			Debug.Log("la");
+			transform.position = new Vector3(transform.position.x, transform.position.y,9);
+		}
+	}
+}
