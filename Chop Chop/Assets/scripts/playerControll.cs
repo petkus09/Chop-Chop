933113b5dba7dca84b883f5db65d750b7d497fbb
ignas <ignasjas3@gmail.com>
2016-01-30 19:39:09 +0200

    g

--- Chop Chop/Assets/scripts/playerControll.cs
+++ Chop Chop/Assets/scripts/playerControll.cs
@@ -18,7 +18,7 @@ public class playerControll : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		GetComponent<Rigidbody> ().AddForce (new Vector3 (2,0,0));
+		GetComponent<Rigidbody> ().AddForce (new Vector3 (1,0,0));
 		GetComponent<Rigidbody> ().velocity = Vector3.ClampMagnitude (GetComponent<Rigidbody> ().velocity, maxVelocity);
 		if (Input.GetKeyDown ("up") && canJump == true) {
 			GetComponent<Rigidbody> ().AddForce (new Vector3 (0,1100,0));
@@ -27,7 +27,7 @@ public class playerControll : MonoBehaviour {
 
 	}
 	void OnCollisionEnter(Collision col) {
-		GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 250, 0));
+		GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 150, 0));
 		blood.Emit (30);
 		canJump = true;
 		if (col.gameObject.tag == "vaze") {
