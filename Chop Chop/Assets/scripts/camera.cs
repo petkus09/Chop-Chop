e4aa686f16f530f60c1f568ca562bc72f551cdc0
ignas <ignasjas3@gmail.com>
2016-01-30 17:07:15 +0200

    j

--- Chop Chop/Assets/scripts/camera.cs
+++ Chop Chop/Assets/scripts/camera.cs
@@ -4,10 +4,7 @@ using System.Collections;
 public class camera : MonoBehaviour {
 
 	public GameObject head;
-<<<<<<< HEAD
 
-=======
->>>>>>> fc681e501ed9ffa5e602114f7ea0e043b812155c
 
 	// Use this for initialization
 	void Start () {
@@ -17,16 +14,9 @@ public class camera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position = Vector3.Slerp (new Vector3 (transform.position.x, transform.position.y, transform.position.z),
-<<<<<<< HEAD
-=======
+
 			new Vector3 (head.transform.position.x + 19f, head.transform.position.y + 13f , transform.position.z), 3 * Time.deltaTime);
->>>>>>> fc681e501ed9ffa5e602114f7ea0e043b812155c
 
-			new Vector3 (head.transform.position.x + 28f, head.transform.position.y + 9f , transform.position.z), 3 * Time.deltaTime);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> fc681e501ed9ffa5e602114f7ea0e043b812155c
 	}
 }
---
+++ Chop Chop/Assets/scripts/scoreHealthCanvas.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class scoreHealthCanvas : MonoBehaviour {
+
+
+	public int healthLeft = 3;
+	public Image health1;
+	public Image health2;
+	public Image health3;
+	public float score = 0;
+	public Text VisualScore;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+			
+		score += 15 * Time.deltaTime;
+		VisualScore.text = Mathf.RoundToInt(score) + "";
+		if(healthLeft == 2) {
+			health3.gameObject.SetActive(false);
+		}
+		if(healthLeft == 1) {
+			health2.gameObject.SetActive(false);
+		}
+		if(healthLeft == 0) {
+			health1.gameObject.SetActive(false);
+		}
+	}
+}
