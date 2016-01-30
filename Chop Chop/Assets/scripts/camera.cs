c4496b1023ee7f0e82a650b7b88c748f19970a78
Tautvydas Petkus <petkus09@gmail.com>
2016-01-30 14:51:24 +0200

    merging conflicts

--- Chop Chop/Assets/scripts/camera.cs
+++ Chop Chop/Assets/scripts/camera.cs
@@ -4,10 +4,6 @@ using System.Collections;
 public class camera : MonoBehaviour {
 
 	public GameObject head;
-<<<<<<< HEAD
-=======
-	Vector3 lastPosition = Vector3.zero;
->>>>>>> 6b3be8927b7ac0ed33c809bd9fbecaa156ffa846
 
 	// Use this for initialization
 	void Start () {
@@ -17,14 +13,9 @@ public class camera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position = Vector3.Slerp (new Vector3 (transform.position.x, transform.position.y, transform.position.z),
-<<<<<<< HEAD
 			new Vector3 (head.transform.position.x + 19f, head.transform.position.y + 13f , transform.position.z), 3 * Time.deltaTime);
 
 			
 
-
-=======
-			new Vector3 (head.transform.position.x + 19f, head.transform.position.y +13f , transform.position.z), 5 * Time.deltaTime);
->>>>>>> 6b3be8927b7ac0ed33c809bd9fbecaa156ffa846
 	}
 }
